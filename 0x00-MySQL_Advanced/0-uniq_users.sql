a28891cefe9ccd5a8973dbebf16fe583f884ea33
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 02:14:25 +0100

    :card_file_box: dropped table if exists

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- Creates a table with unique users.
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NUll PRIMARY KEY AUTO_INCREMENT,
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NUll AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- Creates a table with enumerated countries.
-
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NUll AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
