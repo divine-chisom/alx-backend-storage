6db5ea51657e0867ed0b16cf14570b47a73ccbdc
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 22:25:22 +0100

    :card_file_box: process for weighted average score for all users

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,35 +1,36 @@
 -- Creates a stored procedure ComputeAverageWeightedScoreForUsers that
 -- computes and store the average weighted score for all students.
+
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    ALTER TABLE users ADD total_weighted_score INT NOT NULL;
-    ALTER TABLE users ADD total_weight INT NOT NULL;
+    ALTER TABLE users ADD weighted_score_sum INT NOT NULL;
+    ALTER TABLE users ADD weight_sum INT NOT NULL;
 
     UPDATE users
-        SET total_weighted_score = (
+        SET weighted_score_sum = (
             SELECT SUM(corrections.score * projects.weight)
             FROM corrections
-                INNER JOIN projects
+                JOIN projects
                     ON corrections.project_id = projects.id
             WHERE corrections.user_id = users.id
             );
 
     UPDATE users
-        SET total_weight = (
+        SET weight_sum = (
             SELECT SUM(projects.weight)
                 FROM corrections
-                    INNER JOIN projects
+                    JOIN projects
                         ON corrections.project_id = projects.id
                 WHERE corrections.user_id = users.id
             );
 
     UPDATE users
-        SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
+        SET users.average_score = users.weighted_score_sum / users.weight_sum;
     ALTER TABLE users
-        DROP COLUMN total_weighted_score;
+        DROP COLUMN weighted_score_sum;
     ALTER TABLE users
-        DROP COLUMN total_weight;
+        DROP COLUMN weight_sum;
 END $$
 DELIMITER ;
