1ac760c8d63b8060c8a16d7fadaec7efca4a3203
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 11:42:12 +0100

    :card_file_box: made code more readable

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Creates a table with unique users.
+
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NUll AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
     FROM metal_bands
     WHERE style LIKE '%Glam rock%'
