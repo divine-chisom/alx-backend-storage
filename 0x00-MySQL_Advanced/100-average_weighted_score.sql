c14aa3da8f2c609edab3c8e951d6dc4091649511
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 21:34:10 +0100

    :card_file_box: process for weighted average score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,33 +1,28 @@
 -- creates a stored procedure ComputeAverageWeightedScoreForUser that
 -- computes and store the average weighted score for a student.
+
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_weighted_score INT DEFAULT 0;
-    DECLARE total_weight INT DEFAULT 0;
+    DECLARE weighted_score_sum INT DEFAULT 0;
+    DECLARE weight_sum INT DEFAULT 0;
 
     SELECT SUM(corrections.score * projects.weight)
-        INTO total_weighted_score
+        INTO weighted_score_sum
         FROM corrections
-            INNER JOIN projects
+            JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
 
     SELECT SUM(projects.weight)
-        INTO total_weight
+        INTO weight_sum
         FROM corrections
-            INNER JOIN projects
+            JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
-
-    IF total_weight = 0 THEN
         UPDATE users
-            SET users.average_score = 0
-            WHERE users.id = user_id;
-    ELSE
-        UPDATE users
-            SET users.average_score = total_weighted_score / total_weight
+            SET users.average_score = weighted_score_sum / weight_sum
             WHERE users.id = user_id;
     END IF;
 END $$
