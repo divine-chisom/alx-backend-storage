2993add8c77e20f3eca0e2222e58e1d25ee19a96
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 12:31:18 +0100

    :card_file_box: removed delimiter

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,13 +2,9 @@
 -- of an item after adding a new order.
 
 DROP TRIGGER IF EXISTS decrease_item;
-DELIMITER $$
 CREATE TRIGGER IF NOT EXISTS decrease_item
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
         SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
-END $$
-DELIMITER ;
