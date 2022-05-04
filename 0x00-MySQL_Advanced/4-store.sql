c44cb1bb7726e3a48786baaf56befa1030a743d4
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 12:35:55 +0100

    :card_file_box: removed condition on create statement

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,13 @@
 -- Creates a trigger that decreases the quantity
 -- of an item after adding a new order.
-
 DROP TRIGGER IF EXISTS decrease_item;
-CREATE TRIGGER IF NOT EXISTS decrease_item
+DELIMITER $$
+CREATE TRIGGER decrease_item
 AFTER INSERT ON orders
 FOR EACH ROW
+BEGIN
     UPDATE items
         SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
