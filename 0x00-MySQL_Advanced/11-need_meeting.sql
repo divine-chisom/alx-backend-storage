051ccb0e37f19056fb22a81f5086b5ffe178414b
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 18:07:07 +0100

    :card_file_box: view student's score and meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,6 @@
 -- Creates a view need_meeting that lists all students that have a score
 -- under 80 (strict) and no last_meeting or more than 1 month.
+
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name
@@ -7,6 +8,6 @@ CREATE VIEW need_meeting AS
         WHERE score < 80 AND
             (
                 last_meeting IS NULL
-                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+                OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH)
             )
 ;
