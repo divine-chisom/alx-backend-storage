b71863ae3ecd447f0e6e95893dc2ca72901bca70
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-04 01:35:53 +0100

    :card_file_box: Created a table of unique users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creates a table with unique users.
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NUll PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
