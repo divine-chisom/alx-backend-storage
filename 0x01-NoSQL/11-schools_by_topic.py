70b10eaa35cae9f893b7af68b7a49ab08e5dd7a6
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-09 14:23:31 +0100

    :card_file_box:  list of school having a specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-'''Task 11's module.
+'''11. Where can I learn Python?
 '''
 
 
 def schools_by_topic(mongo_collection, topic):
     '''Returns the list of school having a specific topic.
     '''
-    topic_filter = {
-        'topics': {
-            '$elemMatch': {
-                '$eq': topic,
-            },
-        },
-    }
-    return [doc for doc in mongo_collection.find(topic_filter)]
+    return mongo_collection.find({"topics": topic})
