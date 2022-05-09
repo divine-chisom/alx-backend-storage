5b1fa55e251c23cb2c2e0e2c679a5299074c0820
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-09 17:11:04 +0100

    :card_file_box: student's average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,23 +6,13 @@
 def top_students(mongo_collection):
     '''Prints all students in a collection sorted by average score.
     '''
-    students = mongo_collection.aggregate(
-        [
-            {
-                '$project': {
-                    '_id': 1,
-                    'name': 1,
-                    'averageScore': {
-                        '$avg': {
-                            '$avg': '$topics.score',
-                        },
-                    },
-                    'topics': 1,
-                },
-            },
-            {
-                '$sort': {'averageScore': -1},
-            },
-        ]
-    )
-    return students
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                '_id': 1,
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
